154f218a5bce896caee260c598450c71a2ba7fb2
J.Dondlinger <dondlingergeneralcontracting@gmail.com>
2026-07-13 14:53:59 -0500

    fix: Compile Blazor PWA and serve at root to replace static layout

--- JobsiteCalculatorPwa/build.ps1
+++ JobsiteCalculatorPwa/build.ps1
@@ -1,5 +1,12 @@
 cd c:\Users\John\Desktop\dondlingergc.com\JobsiteCalculatorPwa
 if (Test-Path ../publish_temp) { Remove-Item -Recurse -Force ../publish_temp }
 dotnet publish -c Release -o ../publish_temp
-Remove-Item -Recurse -Force ../calc/*
-Copy-Item -Recurse ../publish_temp/wwwroot/* ../calc/
+
+if (Test-Path ../index.html) {
+    if (-not (Test-Path ../_workspace_archive)) { New-Item -ItemType Directory -Path ../_workspace_archive }
+    Move-Item -Force ../index.html ../_workspace_archive/index.html
+}
+
+if (Test-Path ../_framework) { Remove-Item -Recurse -Force ../_framework }
+
+Copy-Item -Recurse -Force ../publish_temp/wwwroot/* ../
