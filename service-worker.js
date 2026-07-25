8f0cd2f7bb0448dcea6c2ea293a10daf797cea78
J.Dondlinger <dondlingergeneralcontracting@gmail.com>
2026-07-25 07:37:08 -0500

    Emergency Rollback: Restore 3-plate index.html

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: bBUDXFAU */
+/* Manifest version: 7hZqxBYN */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
